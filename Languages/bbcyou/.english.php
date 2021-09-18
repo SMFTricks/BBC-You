3c427f62147c2d82fe1f716f1e88dace7cd37c7b
Diego Andrés <diegoandres_cortes@outlook.com>
2021-09-18 13:50:14 -0500

    add button and setting
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Languages/bbcyou/.english.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package BBC You
+ * @version 2.2
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2021, Diego Andrés
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+$txt['bbc_you'] = 'Inserts the name of the person reading';
+$txt['bbc_you_setting'] = 'Enable bbc [you] icon in the editor';
+$txt['bbc_you_setting_desc'] = 'The BBC will still work, this is just to display a button for easy use.';
--- Sources/BBC-You.php
+++ Sources/BBC-You.php
@@ -2,9 +2,9 @@
 
 /**
  * @package BBC You
- * @version 2.1
+ * @version 2.2
  * @author Diego Andrés <diegoandres_cortes@outlook.com>
- * @copyright Copyright (c) 2015, Diego Andrés
+ * @copyright Copyright (c) 2021, Diego Andrés
  * @license https://www.mozilla.org/en-US/MPL/2.0/
  */
 
@@ -17,11 +17,42 @@ class BBCYou
 	{
 		global $user_info, $txt;
 
-		$codes[] = array(
+		$codes[] = [
 				'tag' => 'you',
 				'type' => 'closed',
 				'content' => empty($user_info['is_guest']) && !empty($user_info['name']) ? $user_info['name'] : (!empty($txt[28]) ? $txt[28] : (!empty($txt['guest']) ? $txt['guest'] : 'Guest')),
-		);
+		];
+	}
 
+	public static function settings(&$bbc_settings)
+	{
+		global $txt;
+
+		// Load language
+		loadLanguage('bbcyou/');
+
+		$bbc_settings[] = [
+			'check', 'bbc_you_setting', 'subtext' => $txt['bbc_you_setting_desc']
+		];
+	}
+
+	public static function button(&$tags)
+	{
+		global $txt, $modSettings;
+
+		// Check if it's enabled
+		if (empty($modSettings['bbc_you_setting']))
+			return;
+
+		// Load language
+		loadLanguage('bbcyou/');
+
+		// Add the button
+		$tags[][] = [
+			'image' => 'you',
+			'code' => 'you',
+			'before' => '[you]',
+			'description' => $txt['bbc_you']
+		];
 	}
 }
